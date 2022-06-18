cd8ed50c256be2dd28de987d061e64e1f7b5376a
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-06-18 16:51:52 +0900

    verify id_token with google-id-token gem

--- web/app/models/google_auth.rb
+++ web/app/models/google_auth.rb
@@ -1,5 +1,3 @@
-require 'net/http'
-
 class GoogleAuth
   class << self
     def verify(id_token)
@@ -10,16 +8,11 @@ class GoogleAuth
     end
 
     def fetch_token_info(token)
-      # NOTE: This way is not suitable for use in production code.
-      # SEE: https://developers.google.com/identity/sign-in/android/backend-auth#calling-the-tokeninfo-endpoint
-      url = URI("https://oauth2.googleapis.com/tokeninfo")
-      res = Net::HTTP.post(url, { 'id_token' => token }.to_json, 'Content-Type' => 'application/json')
-      if res.code == "200"
-        JSON.parse(res.body)
-      else
-        Rails.logger.info("Failed to verify token, code=#{res.code}, body=#{res.body}")
-        nil
-      end
+      validator = GoogleIDToken::Validator.new
+      validator.check(token, '<YOUR OAUTH CLIENT ID HERE>')
+    rescue GoogleIDToken::ValidationError => e
+      Rails.logger.info("Failed to verify token, #{e}")
+      nil
     end
   end
 end
